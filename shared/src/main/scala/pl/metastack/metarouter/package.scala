646bdef7faa5fee77ec8086a9153acc07d4d8c2d
Tim Nieradzik <tim@metastack.pl>
2016-02-03 09:01:05 +0100

    Package: Remove unused imports

--- shared/src/main/scala/pl/metastack/metarouter/package.scala
+++ shared/src/main/scala/pl/metastack/metarouter/package.scala
@@ -1,11 +1,6 @@
 package pl.metastack
 
-import pl.metastack.metarouter.Route.ConvertArgs
-import shapeless.HNil
-import shapeless.ops.hlist.FlatMapper
-import shapeless.ops.hlist.FlatMapper._
-
 package object metarouter {
   val Root = Route.Root
-  val !# = Root
+  val !#   = Root
 }
