771a2b317a1ceeac130f7349cacf7f583bc9d722
Tim Nieradzik <tim@metastack.pl>
2016-06-21 21:59:16 +0200

    Route: Move Arg to separate file

--- shared/src/main/scala/pl/metastack/metarouter/Route.scala
+++ shared/src/main/scala/pl/metastack/metarouter/Route.scala
@@ -1,7 +1,6 @@
 package pl.metastack.metarouter
 
 import scala.annotation.implicitNotFound
-import scala.util.Try
 
 import shapeless._
 import shapeless.ops.hlist._
@@ -204,23 +203,3 @@ trait StaticElement[T] extends PathElement[T] {
   def toPathElement(t: T): String = urlEncode(t)
 }
 
-case class Arg[T](implicit val parseableArg: ParseableArg[T])
-
-trait ParseableArg[T] {
-  def urlDecode(s: String): Option[T]
-  def urlEncode(s: T): String
-}
-object ParseableArg {
-  implicit case object BooleanArg extends ParseableArg[Boolean] {
-    override def urlDecode(s: String) = Try(s.toBoolean).toOption
-    override def urlEncode(s: Boolean) = s.toString
-  }
-  implicit case object IntArg extends ParseableArg[Int] {
-    override def urlDecode(s: String) = Try(s.toInt).toOption
-    override def urlEncode(s: Int) = s.toString
-  }
-  implicit case object StringArg extends ParseableArg[String] {
-    override def urlDecode(s: String) = Option(s)
-    override def urlEncode(s: String) = s
-  }
-}
