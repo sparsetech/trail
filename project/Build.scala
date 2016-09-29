fbc478d04cb2af66d5c9da085a42e308c6228f45
Tim Nieradzik <tim@metastack.pl>
2016-09-29 19:13:06 +0300

    Remove unused Build.scala

--- project/Build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-import sbt.Keys._
-import xerial.sbt.Sonatype.sonatypeSettings
-import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
-import sbtbuildinfo.BuildInfoPlugin
-import sbtbuildinfo.BuildInfoPlugin.autoImport._
-
-object Build extends sbt.Build {
-}
