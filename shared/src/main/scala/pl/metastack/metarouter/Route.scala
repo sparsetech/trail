4209ef796706188683446e44b472382b63012669
Tim Nieradzik <tim@sparse.tech>
2017-01-20 13:46:50 +0100

    Route: Remove `private` qualifiers

--- shared/src/main/scala/pl/metastack/metarouter/Route.scala
+++ shared/src/main/scala/pl/metastack/metarouter/Route.scala
@@ -7,11 +7,11 @@ object Route {
   val Root = Route[HNil](HNil)
 }
 
-case class Route[ROUTE <: HList] private[metarouter] (pathElements: ROUTE) {
+case class Route[ROUTE <: HList](pathElements: ROUTE) {
   def /[T, E](a: T)(implicit pe: PathElement.Aux[T, E], prepend: Prepend[ROUTE, E :: HNil]) =
     Route(pathElements :+ pe.toPathElement(a))
 }
 
-case class RouteData[ROUTE <: HList, DATA <: HList] private[metarouter] (route: Route[ROUTE], data: DATA)
+case class RouteData[ROUTE <: HList, DATA <: HList](route: Route[ROUTE], data: DATA)
 
-case class MappedRoute[ROUTE <: HList, T] private[metarouter] (route: Route[ROUTE])
+case class MappedRoute[ROUTE <: HList, T](route: Route[ROUTE])
